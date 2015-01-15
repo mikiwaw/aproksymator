6e869c3b1d3b0c6942ee3239e9a0a1a1268390ae
korsarzPC <mikiwaw@gmail.com>
2015-01-15 01:27:18 +0100

    -liczenie macierzy odwrotnej zastapilem dekompozycja LU - wieksza wydajnosc

--- aproksymator_legendre/aproksymator_legendre.c
+++ aproksymator_legendre/aproksymator_legendre.c
@@ -1,17 +1,18 @@
 #include "makecof.h"
 
 #include <math.h>
-#include <float.h>
+//#include <float.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_linalg.h>
-#include <gsl/gsl_cblas.h>
-#include <gsl/gsl_blas.h>
+//#include <gsl/gsl_cblas.h>
+//#include <gsl/gsl_blas.h>
 
 void  make_cof(points_t *pts, coefficients_t *cof)
 {
 
-    gsl_matrix *A, *A_inverse, *BF, *wsp;
-    gsl_permutation * perm;
+    gsl_matrix *A;
+    gsl_permutation *perm;
+    gsl_vector *wsp, *BF;
 
     double		*x = pts->x;
     double		*y = pts->y;
@@ -19,6 +20,7 @@ void  make_cof(points_t *pts, coefficients_t *cof)
     double		b = x[pts->n - 1];
     int			m = pts->n - 2 > 10 ? 10 : pts->n - 2;
     int			i, j, k;
+    double      tmp;
     char		*mEnv = getenv("APPROX_BASE_SIZE");
 
     if (mEnv != NULL && atoi(mEnv) > 0)
@@ -26,10 +28,9 @@ void  make_cof(points_t *pts, coefficients_t *cof)
     m++;
 
     A = gsl_matrix_alloc(m, m);
-    A_inverse = gsl_matrix_alloc(m, m);
+    wsp = gsl_vector_alloc(m);
+    BF = gsl_vector_alloc(m);
     perm = gsl_permutation_alloc(m);
-    BF = gsl_matrix_alloc(m, 1);
-    wsp = gsl_matrix_alloc(m, 1);
 
     gsl_matrix_set_zero (A);
     for (i = 0; i < m; i++)
@@ -42,31 +43,30 @@ void  make_cof(points_t *pts, coefficients_t *cof)
             }
         }
     }
-//macierz odwrotna
-    gsl_linalg_LU_decomp(A, perm, &i);
-    gsl_linalg_LU_invert(A, perm, A_inverse);
-    gsl_matrix_free(A);
-    gsl_permutation_free(perm);
 
-
-
-    gsl_matrix_set_zero (BF);
+    gsl_vector_set_zero (BF);
     for (i = 0; i < m; i++)
     {
+        tmp = 0.0;
         for (k = 0; k < pts->n; k++)
-            BF->data[i * BF->tda + 0] += legendre(i, x[k]) * y[k];
+            tmp += legendre(i, x[k]) * y[k];
+        gsl_vector_set(BF, i, tmp);
     }
 
-//mnozenie macierzy
-    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, A_inverse, BF, 0.0, wsp);
-    gsl_matrix_free(A_inverse);
-    gsl_matrix_free(BF);
+    gsl_linalg_LU_decomp(A, perm, &i);
+    gsl_linalg_LU_solve(A, perm, BF, wsp);
+
+    gsl_matrix_free(A);
+    gsl_permutation_free(perm);
+    gsl_vector_free(BF);
 
     if (alloc_cof(cof, m) == 0)
     {
         for (i = 0; i < cof->base; i++)
-            cof->coefficients[i] = gsl_matrix_get(wsp, i, 0);
+            cof->coefficients[i] = gsl_vector_get(wsp, i);
     }
+
+    gsl_vector_free(wsp);
 
     return;
 }
